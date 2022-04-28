170e114301fd69d5d77d89f04d3ac96f66da256f
Przemyslaw Bubas <bprzemys@cisco.com>
2022-04-28 22:09:46 +0200

    added more unittests to alert test

--- tests/test_alert.py
+++ tests/test_alert.py
@@ -1,4 +1,5 @@
 from thousandeyessdk.alert import Alert
+from thousandeyessdk.enum import AlertType
 
 from . import ALERT
 
@@ -8,3 +9,43 @@ class TestAlert():
     def test_alert_id(self):
         alert = Alert('api', ALERT, '/alerts')
         assert alert.id == 123456789
+
+    def test_active(self):
+        alert = Alert('api', ALERT, '/alerts')
+        assert alert.active == False
+    
+    def test_inactive(self):
+        alert = Alert('api', ALERT, '/alerts')
+        assert alert.inactive == True
+    
+    def test_disabled(self):
+        alert = Alert('api', ALERT, '/alerts')
+        assert alert.disabled == False
+    
+    def test_rule_expression(self):
+        alert = Alert('api', ALERT, '/alerts')
+        assert alert.rule_expression == "((responseTime >= 300 ms))"
+ 
+    def test_type(self):
+        alert = Alert('api', ALERT, '/alerts')
+        assert alert.type.value == "HTTP Server"
+        assert alert.type.name == "HTTP_SERVER"
+        assert alert.type == AlertType("HTTP Server")
+
+    def test_string_type(self):
+        alert = Alert('api', ALERT, '/alerts')
+        assert alert.string_type == "HTTP Server"
+
+    def test_date_start(self):
+        """date_start not supported currently"""
+        alert = Alert('api', ALERT, '/alerts')
+        assert alert.date_start == None
+
+    def test_date_end(self):
+        """not supported currently"""
+        alert = Alert('api', ALERT, '/alerts')
+        assert alert.date_end == None
+
+
+
+    
