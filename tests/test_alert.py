fb3d2b28d78e18be776573a6563f246eb836b9fa
Przemyslaw Bubas <bprzemys@cisco.com>
2022-05-05 13:12:33 +0200

    corrected comparison operators

--- tests/test_alert.py
+++ tests/test_alert.py
@@ -13,21 +13,21 @@ class TestAlert(TestCase):
         te_client = TE(username=USERNAME, auth_token=AUTH_TOKEN)
         alert = te_client.alerts.get(ALERT_WITH_AGENTS['alert'][0]['alertId'])
         assert alert.id == 111111111
-        assert alert.active == False
-        assert alert.inactive == True
-        assert alert.disabled == False
+        assert alert.active is False
+        assert alert.inactive is True
+        assert alert.disabled is False
         assert alert.rule_expression == "((responseTime >= 300 ms))"
         assert alert.type.value == "HTTP Server"
         assert alert.type.name == "HTTP_SERVER"
         assert alert.type == AlertType("HTTP Server")
         assert alert.string_type == "HTTP Server"
-        assert alert.date_start == None # date_start not supported currently
-        assert alert.date_end == None #d ate_end not supported currently
-        assert alert.violation_count == 2
+        assert alert.date_start is None # date_start not supported currently
+        assert alert.date_end is None # date_end not supported currently
+        assert alert.violation_count is 2
         assert alert.permalink == "https://app.thousandeyes.com/alerts/list/?__a=333666&alertId=111111111"
         agent_names = [name['agentName'] for name in alert.agents]
         assert agent_names == ['AGENT1', 'AGENT2']
-        assert alert.api_links == None #api_links not supported currently
+        assert alert.api_links is None #api_links not supported currently
 
     def test_alert_with_monitors(self,m__request):
         m__request().json.return_value = ALERT_WITH_MONITORS
