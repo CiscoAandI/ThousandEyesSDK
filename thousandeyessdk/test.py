d02e29abad1fe0646d2056e8e081a4938c1c0700
ktrzcins <ktrzcins@cisco.com>
2022-07-05 14:03:34 +0200

    SSE-1614 update docstring of get_detailed_path_trace

--- thousandeyessdk/test.py
+++ thousandeyessdk/test.py
@@ -31,6 +31,17 @@ class Test(BaseEntity):
         return self._data.get('countryId', None)
 
     def get_detailed_path_trace(self, agent_id, round_id):
+        """
+        Request
+            Required parameters:
+
+            {agentId} the ID of the endpoint agent from which you wish to obtain data
+            {roundId} the round ID for which you wish to obtain data. Equals the beginning of the testing round, in epoch time format.
+            There is no request body for this request.
+        Response
+            Each route should start with a hop of 1
+            Where a hop number is missing from response data, this is an indication that a star (*) response was returned in the path trace attempt for that hop.
+        """
         if self._api.version != 6:
             return {}
 
